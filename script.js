4344bccc7a3d9b4e6fb96f66ca135e735d43bfa4
David <davidbensaxofonista@gnail.com>
2023-12-28 23:49:30 -0300

    adicionado um calendário

--- script.js
+++ script.js
@@ -7,12 +7,32 @@ const relogio = setInterval(function time() {
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
+    
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
 
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (s < 10) s = '0' + s;
 
-    horas.textContent = hr;
-    minutos.textContent = min;
-    segundos.textContent = s;
+});
+
+const dia = document.getElementById('dia');
+const mes = document.getElementById('mes');
+const ano = document.getElementById('ano');
+
+const calendario = setInterval(function time() {
+    let dateObj = new Date();
+    let dd = dateObj.getDate();
+    var mm = dateObj.getMonth() +1;
+    var yyyy = dateObj.getFullYear();
+
+    dia.textContent = dd;
+    mes.textContent = mm;
+    ano.textContent = yyyy;
+ 
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    
 });
